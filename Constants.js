7a6ae8a2ded80c7de2ff5f390660991593fab8d5
Federico <fedeojeda95@gmail.com>
2017-11-14 21:30:53 -0300

    created the basic slackbot that responds to the recieved messages

---
+++ Constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  SLACK_API_KEY: 'xoxb-271873980737-wfHXbgtnJDDn3rBBvhJo2GH6'
+};
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+<<<<<<< HEAD
 require('dotenv').config()
 
 const SentimentBot = require('./src/SentimentBot');
 
 const sentimentBot = new SentimentBot();
 sentimentBot.run();
+=======
+const constants = require('./Constants');
+const Slackbot = require('./Slackbot');
+const HttpService = require('./HttpService');
+
+const slackbot = new Slackbot(constants.SLACK_API_KEY);
+
+slackbot
+  .onMessage()
+  .do(message => console.log(message))
+  .subscribe(message => {
+    slackbot.sendMessage('Hello from slack!', message.channel);
+  });
+
+slackbot.start();
+>>>>>>> created the basic slackbot that responds to the recieved messages
